266efd1ce510aa623639c58d6b7a5b3878e16b1f
Karneet Arora <karneetarora@gmail.com>
2026-09-20 22:26:25 -0400

    clean up app structure and start homepage development

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,11 +10,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={``}>
+      <body className="">
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className="w-full text-center py-2 border-t-2 md:py-4">
+            <p>Footer</p>
+        </footer>
+    )
+}
